70a0e2d327740cf8aa8594c36aef2dae14ba454a
Rafael Coelho <rafacoelhoas@gmail.com>
2024-01-19 20:48:04 -0300

    Armazenando os dados no localstorage

--- speedometer.js
+++ speedometer.js
@@ -1,15 +1,36 @@
-const speedElement= document.querySelector("#speed")
-const startBtn= document.querySelector("#start")
-const stopBtn= document.querySelector("#stop")
+const speedElement = document.querySelector("#speed")
+const startBtn = document.querySelector("#start")
+const stopBtn = document.querySelector("#stop")
 
-startBtn.addEventListener("click",() => {
+let watchID = null
+let currentRide = null
+startBtn.addEventListener("click", () => {
+    if (watchID)
+        return
 
+    function handleSucess(position) {
+        addPosition(currentRide, position)
+        console.log(position)
+        speedElement.innerText = position.coords.speed ?
+            (position.coords.speed * 3.6).toFixed(1) : 0
+    }
+    function handleError(error) {
+        console.log(error.msg)
+    }
+
+    const options = { enableHighAccuracy: true }
+    watchID = navigator.geolocation.watchPosition(handleSucess, handleError)
+    currentRide = createNewRide()
     startBtn.classList.add("d-none")
     stopBtn.classList.remove("d-none")
 })
 
-stopBtn.addEventListener("click",() => {
-
+stopBtn.addEventListener("click", () => {
+    if (!watchID)
+        return
+    navigator.geolocation.clearWatch(watchID)
+    watchID = null
+    updateStopTime(currentRide)
     startBtn.classList.remove("d-none")
     stopBtn.classList.add("d-none")
 
